bbe720f482ef5dad33e081026f0c44b78652444f
WalterCan <walterdaniel1316@gmail.com>
2021-11-05 17:16:30 -0300

    Seleccionar elementos : QuerySelector

---
+++ JavaScript_DOM_Eventos_inicio/js/01-Script.js
@@ -0,0 +1,13 @@
+//DOM Scripting
+// Seleecionar contenido, Hay tres formas de hacerlo
+// Siempre se usa un ID para referirnos a un elemento del HTML
+
+// QuerySelector
+//Va a Retornar, ya sea ninguno o hasta un elemnto que concuerde con el selector que estamos escribiendo
+// Son selectores muy similares a los de CSS3
+const heading = document.querySelector(".header__texto h2");
+
+console.log(heading);
+// QuerySelectorAll
+
+// getElemenById
