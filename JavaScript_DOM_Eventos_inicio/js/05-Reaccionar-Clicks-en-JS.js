6cb26bf157897eb592011c17f74ee34abfb40ffa
WalterCan <walterdaniel1316@gmail.com>
2021-11-08 17:50:10 -0300

    Evento de Click

---
+++ JavaScript_DOM_Eventos_inicio/js/05-Reaccionar-Clicks-en-JS.js
@@ -0,0 +1,7 @@
+// Reacción a un evento de click
+// Vamos a Seleccionar un elemento Del HTML y Registrarle un Evento
+
+//
+
+const btnEnviar = document.querySelector(".boton--primario");
+console.log(btnEnviar);
