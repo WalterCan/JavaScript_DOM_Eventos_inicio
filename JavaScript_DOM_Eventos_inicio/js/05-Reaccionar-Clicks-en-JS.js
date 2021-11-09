d7b52a37478402dcaef77310aa1fa60610bfd033
WalterCan <walterdaniel1316@gmail.com>
2021-11-09 09:31:51 -0300

    Evento de Click - preventDefault()

--- JavaScript_DOM_Eventos_inicio/js/05-Reaccionar-Clicks-en-JS.js
+++ JavaScript_DOM_Eventos_inicio/js/05-Reaccionar-Clicks-en-JS.js
@@ -5,10 +5,9 @@
 
 const btnEnviar = document.querySelector(".boton--primario");
 btnEnviar.addEventListener("click", function (evento) {
-  //Pasamos como parametro evento, hace referencia al evento que está pasando
+  //pasamos el parametro de Evento
   console.log(evento);
-  evento.preventDefault();
+  evento.preventDefault(); //Previene la accion por Default -> Es util para validar un Formulario (Que todos los campos esten llenos para enviar el formulario)
   console.log("Enviando Formulario");
 });
-
 // Cuando presionamos en el submit, su funcion por default es enviar esa información.
